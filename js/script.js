11ffaf6d904c1af5d59bf9262b0a030c3513fc5f
Nikita <nikitazapletin@gmail.com>
2022-09-05 16:07:21 +0300

    first lesson

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 "use strict";
 
-let check = confirm(`Are you here?`);
+let numberOfFilms = +prompt(`Сколько фильмов вы уже посмотрели?`, ``);
 
-console.log(check);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+
+const a = prompt(`Один из последних просмотренных фильмов?`, ``),
+      b = prompt(`На сколько его оцените?`, ``),
+      c = prompt(`Один из последних просмотренных фильмов?`, ``),
+      d = prompt(`На сколько его оцените?`, ``);
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
